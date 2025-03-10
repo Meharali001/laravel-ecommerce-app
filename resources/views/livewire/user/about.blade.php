40820ddca44bf054623f98a96e60597333c16775
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2025-03-11 01:52:08 +0500

    some changes

--- resources/views/livewire/user/about.blade.php
+++ resources/views/livewire/user/about.blade.php
@@ -6,7 +6,17 @@
         <div class="card-body text-center">
             <img src="{{ asset('assets/admin/images/icon/Untitled-1.jpg') }}" alt="Profile Picture" class="rounded-circle" width="150">
             <h3 class="mt-3">Your Name</h3>
-            <p class="text-muted">Web Developer | Laravel Expert | Freelancer</p>
+            <p class="text-muted">Web Developer || Laravel Expert || Freelancer || Database Architect || CMS, LMS, CRM, CRMS, HRM Developer.</p>
+            
+            <p class="mt-3">
+                <a rel="noopener noreferrer" class="text-success fw-bold" href="https://github.com/Meharali001" target="_blank">
+                    My GitHub Profile
+                </a> | 
+                <a rel="noopener noreferrer" class="text-success fw-bold" href="https://www.linkedin.com/in/mohammed-hasnain-7aa110315/" target="_blank">
+                    LinkedIn
+                </a>
+            </p>
+
             <p class="mt-3">
                 Welcome to my page! I am a passionate developer who loves coding in Laravel and Vue.js.
                 I specialize in building web applications, integrating APIs, and developing modern, scalable solutions.
